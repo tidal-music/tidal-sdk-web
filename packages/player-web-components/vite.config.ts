ebc50e18c38989140a0a07a7baa0031d7819bc15
Haff <TheHaff@users.noreply.github.com>
2024-02-08 18:09:12 -0500

    try vitest coverage gh action

--- packages/player-web-components/vite.config.ts
+++ packages/player-web-components/vite.config.ts
@@ -16,7 +16,14 @@ export default defineConfig({
   },
   test: {
     coverage: {
+      reportOnFailure: true,
       reporter: process.env.CI ? ['json', 'json-summary'] : ['html'],
+      thresholds: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80,
+      },
     },
     globals: true,
     restoreMocks: true,
