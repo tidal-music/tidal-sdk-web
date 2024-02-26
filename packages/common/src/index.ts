2ba50b4d9a48c80466745c28102f67377f81187a
Haff <TheHaff@users.noreply.github.com>
2024-02-26 16:48:20 -0500

    ignore coverage for exports

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,6 +1,8 @@
+/* v8 ignore start */
 export * from './illegalArgumentError';
 export * from './networkError';
 export * from './retryableError';
 export * from './tidalError';
 export * from './tidalMessage';
 export * from './credentialsProvider';
+/* v8 ignore stop */
