526ef164b940c15242a4cebc2a123c724707b1fd
Haff <TheHaff@users.noreply.github.com>
2024-02-28 09:48:17 -0500

    remove v8 ignore

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,8 +1,6 @@
-/* v8 ignore start */
 export * from './illegalArgumentError';
 export * from './networkError';
 export * from './retryableError';
 export * from './tidalError';
 export * from './tidalMessage';
 export * from './credentialsProvider';
-/* v8 ignore stop */
