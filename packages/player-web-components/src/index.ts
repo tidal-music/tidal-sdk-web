9200397a1d114a2ba76a9a4a3b99870109a091b4
Jeremy Karlsson <jeremyk@tidal.com>
2024-05-29 10:28:31 +0200

    Expose bootstrap method

--- packages/player-web-components/src/index.ts
+++ packages/player-web-components/src/index.ts
@@ -4,4 +4,4 @@ export { default as TidalPlayButton } from './tidal-play-trigger';
 export { default as TidalProgressBar } from './tidal-progress-bar';
 export { default as TidalVideoView } from './tidal-video-view';
 export type { CredentialsProvider } from '@tidal-music/common';
-export { events, setCredentialsProvider } from '@tidal-music/player';
+export { bootstrap, events, setCredentialsProvider } from '@tidal-music/player';
