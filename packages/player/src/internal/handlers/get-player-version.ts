2bac8018bfbddb2a070f4f3d7a1f5e7739c587c8
Jeremy Karlsson <jeremyk@tidal.com>
2024-10-03 11:24:11 +0200

    Fix types

--- packages/player/src/internal/handlers/get-player-version.ts
+++ packages/player/src/internal/handlers/get-player-version.ts
@@ -1,3 +1,6 @@
-export function getPlayerVersion() {
+/**
+ * Get the module version of the Player SDK.
+ */
+export function getPlayerVersion(): string {
   return import.meta.env.VITE_PLAYER_VERSION;
 }
