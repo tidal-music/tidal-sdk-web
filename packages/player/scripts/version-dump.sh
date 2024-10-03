6fcb1d25ecf0ce2524f7f11aa85899fee14c0774
Jeremy Karlsson <jeremyk@tidal.com>
2024-10-03 11:27:38 +0200

    Add comment in .env file

--- packages/player/scripts/version-dump.sh
+++ packages/player/scripts/version-dump.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
-# Navigate to the directory containing the script
 cd "$(dirname "$0")"
 
-# Read the version from package.json using grep and sed
 VERSION=$(grep '"version"' ../package.json | sed -E 's/.*"version": "([^"]+)".*/\1/')
 
-# Write the version to .env.production
-echo "VITE_PLAYER_VERSION=$VERSION" > ../.env.production
+echo "# Automatically generated from package.json" > ../.env.production
+echo "VITE_PLAYER_VERSION=$VERSION" >> ../.env.production
 
 echo "VITE_PLAYER_VERSION=$VERSION has been written to .env.production"
