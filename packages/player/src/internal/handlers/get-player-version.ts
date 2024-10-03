780eb704efd705f275398856746f4081bd9c1209
Jeremy Karlsson <jeremyk@tidal.com>
2024-10-03 15:56:29 +0200

    Use vite plugin for getting version instead.

--- packages/player/src/internal/handlers/get-player-version.ts
+++ packages/player/src/internal/handlers/get-player-version.ts
@@ -2,5 +2,5 @@
  * Get the module version of the Player SDK.
  */
 export function getPlayerVersion(): string {
-  return import.meta.env.VITE_PLAYER_VERSION;
+  return import.meta.env.PACKAGE_VERSION;
 }
--- packages/player/vite-env.d.ts
+++ packages/player/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_PLAYER_VERSION: string;
+  readonly PACKAGE_VERSION: string;
 }
 
 interface ImportMeta {
