fff3cf516e4abab4d24c142cc4205b7f4271d189
Øyvind Smestad <oyvind@tidal.com>
2025-03-12 15:24:27 +0100

    Switch to one API definition (to rule them all)

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,12 +1,3 @@
+export type { components } from './allAPI.generated';
+
 export { createAPIClient } from './api';
-
-import type { components as catalogueComponents } from './catalogueAPI.generated';
-import type { components as playlistComponents } from './playlistAPI.generated';
-import type { components as searchComponents } from './searchAPI.generated';
-import type { components as userComponents } from './userAPI.generated';
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-export type components = catalogueComponents &
-  playlistComponents &
-  searchComponents &
-  userComponents;
