c16295ce22bfa2f136798de313fd65d46565583b
Øyvind Smestad <oyvind@tidal.com>
2025-04-24 14:43:48 +0200

    Player: Simplify event commit logic and remove unused imports

--- packages/player/src/internal/event-tracking/index.ts
+++ packages/player/src/internal/event-tracking/index.ts
@@ -1,5 +1,5 @@
 import { db } from '../helpers/event-session';
-import { credentialsProviderStore, eventSenderStore } from '../index';
+import { eventSenderStore } from '../index';
 
 type BaseCommitData = {
   group: string;
@@ -12,13 +12,9 @@ type BaseCommitData = {
  * Send event to event system scoped to streaming_metrics category.
  */
 export async function commit(baseCommitData: BaseCommitData) {
-  /*
-    This is a workaround to keep the old nested header structure,
-    turn off when we can rely on the header enrichment solely.
-  */
-  const oldNestedHeader = true;
   const event = {
     consentCategory: 'NECESSARY' as const,
+    ...('extras' in baseCommitData && { extras: baseCommitData.extras }),
     group: baseCommitData.group,
     name: baseCommitData.name,
     payload: baseCommitData.payload,
@@ -31,27 +27,6 @@ export async function commit(baseCommitData: BaseCommitData) {
   // streamingSessionId not allowed in the progress event
   if (event.name === 'progress' && 'streamingSessionId' in event.payload) {
     delete event.payload.streamingSessionId;
-  }
-
-  if (oldNestedHeader) {
-    const credentials =
-      await credentialsProviderStore.credentialsProvider.getCredentials();
-
-    const config = eventSenderStore.eventSender.getConfig();
-
-    // @ts-expect-error - client field in spec, but required apparently...
-    event.client = {
-      platform: config.platform,
-      token: credentials.clientId,
-      version: config.appInfo.appVersion,
-    };
-
-    // @ts-expect-error - user field in spec, but required apparently...
-    event.user = {
-      accessToken: credentials.token,
-      clientId: credentials.clientId,
-      id: credentials.userId,
-    };
   }
 
   eventSenderStore.eventSender.sendEvent(event);
--- packages/player/src/internal/event-tracking/play-log/index.ts
+++ packages/player/src/internal/event-tracking/play-log/index.ts
@@ -1,12 +1,9 @@
 export { playbackSession, playbackSessionAction } from './playback-session';
 
 import type { MediaProduct } from '../../../api/interfaces';
-//import { commit as beaconCommit, commitOpen as beaconCommitOpen, worker } from '../../beacon/index';
-//import type { CommitData } from '../../beacon/types';
-import { runIfAuthorizedWithUser } from '../../helpers/run-if-authorized-with-user';
+import { isAuthorizedWithUser } from '../../index';
 import { commit as baseCommit } from '../index';
 import type { Events } from '../types';
-//import { runConditionalOnAuth } from '../../helpers/run-conditional-on-auth';
 
 import type { PlayLogProductType } from './playback-session';
 
@@ -31,32 +28,19 @@ export function mapProductTypeToPlayLogProductType(
  * Send event to event system scoped to play_log / play_log_open category.
  */
 export async function commit(data: Events) {
-  return runIfAuthorizedWithUser(async () => {
-    for (const event of data) {
-      if (event) {
-        const resolvedEvent = await event;
-        if (resolvedEvent) {
-          await baseCommit({
-            group: 'play_log',
-            name: resolvedEvent.name,
-            payload: resolvedEvent.payload,
-            version: 2,
-          });
-        }
+  const authorizedWithUser = await isAuthorizedWithUser();
+  for (const event of data) {
+    if (event) {
+      const resolvedEvent = await event;
+      if (resolvedEvent) {
+        await baseCommit({
+          ...('extras' in resolvedEvent && { extras: resolvedEvent.extras }),
+          group: authorizedWithUser ? 'play_log' : 'play_log_open',
+          name: resolvedEvent.name,
+          payload: resolvedEvent.payload,
+          version: 2,
+        });
       }
     }
-  });
-  /*
-   return runConditionalOnAuth({
-    auth: () =>
-      beaconCommit(worker, {
-        type: 'play_log' as const,
-        ...data,
-      }),
-    open: () =>
-      beaconCommitOpen(worker, {
-        type: 'play_log_open' as const,
-        ...data,
-      }),
-  }); */
+  }
 }
