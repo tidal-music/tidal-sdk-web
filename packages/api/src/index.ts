c1c22c212cf0237bc58c08a0288e427a1c16edf1
Adi Sahar <adisahar1@hotmail.com>
2026-07-11 04:59:35 +0300

    fix types export

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -6,7 +6,7 @@
  *
  * @ignore
  */
-export type { components } from './allAPI.generated.js';
+export type { paths, components } from './allAPI.generated.js';
 
 export { createAPIClient } from './api.js';
 
